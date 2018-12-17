88bd15ac372024a5f5adfd748cd7a528f2ae3a31
RicardoJ <rjdiazzh@gmail.com>
2018-12-16 21:23:38 -0500

    prueba usando POST

--- src/test/java/co/com/RestAssured/Demo.java
+++ src/test/java/co/com/RestAssured/Demo.java
@@ -8,9 +8,8 @@ import org.junit.Test;
 import io.restassured.RestAssured;
 
 import io.restassured.response.Response;
-import static org.hamcrest.Matchers.equalTo;
-import static org.hamcrest.Matchers.notNullValue;;
 
+import static org.hamcrest.Matchers.*;
 
 public class Demo {
 //	@Before
@@ -18,7 +17,7 @@ public class Demo {
 //	    RestAssured.baseURI ="http://swapi.co/api/people/1";
 //	    RestAssured.port = 443;
 //	}	
-
+/*
 	@Test
 	public void testGET() {
 		String body = RestAssured
@@ -36,11 +35,83 @@ public class Demo {
                     
                     .and().extract().body().asString();
 
+	}*/
+	
+	@Test
+    public void whenRequestingAResourceThenLinksToResourcesMustBeReturned() {
+
+        BaseApiResponse baseApiResponse = RestAssured
+            .given()
+                .baseUri("http://swapi.co/api")
+                .and()
+                .queryParam("format", "json")
+                .log().all()
+            .when()
+                .get("/")
+            .then()
+                .statusCode(is(equalTo(200)))
+                .and()
+                .body("films", response -> notNullValue())
+                .body("vehicles", response -> notNullValue())
+                .body("people", response -> notNullValue())
+                .body("starships", response -> notNullValue())
+                .body("species", response -> notNullValue())
+                .body("planets", response -> notNullValue())
+                .and().extract().body().as(BaseApiResponse.class);
+
+        RestAssured
+            .given()
+                .queryParam("format", "json")
+                .log().all()
+            .when()
+                .post(baseApiResponse.getPeople())
+            .then()
+                .log().all() //log : nos permite configurar el nivel de detalle (de request y response por separado
+                .and()
+                .assertThat()
+                    .statusCode(is(equalTo(405)));
+    }
+
+    private static class BaseApiResponse {
+        private String films;
+        private String vehicles;
+        private String people;
+        private String starships;
+        private String species;
+        private String planets;
+
+        public String getFilms() {
+            return films;
+        }
+
+        public String getVehicles() {
+            return vehicles;
+        }
+
+        public String getPeople() {
+            return people;
+        }
+
+        public String getStarships() {
+            return starships;
+        }
+
+        public String getSpecies() {
+            return species;
+        }
+
+        public String getPlanets() {
+            return planets;
+        }
+    }
+}
 	
 	
 	
-	}
 	
-	}
+	
+	
+	
+	
 
 
